5bdb7dd2efb1edec69298c59761d73c26e83e214
dpardy480 <david_pardy@comcast.com>
2021-04-16 01:22:02 -0600

    first commit

---
+++ src/components/Tabs.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="tabs">
+      <ul>
+        <li v-for="tab in tabs" v-bind:key="tab.name" :class="{ 'is-active': tab.isActive }">
+            <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="tabs-details">
+        <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { tabs: [] };
+  },
+  created() {
+    this.tabs = this.$children;
+  },
+  methods: {
+    selectTab(selectedTab) {
+      this.tabs.forEach((tab) => {
+        const tabElement = tab;
+        tabElement.isActive = (tabElement.name === selectedTab.name);
+      });
+    },
+  },
+};
+</script>
---
+++ src/views/Test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="test">
+    <h1>This is a test page</h1>
+    <tabs>
+      <tab name="Services" :selected="true">
+        <h1>What we do</h1>
+      </tab>
+      <tab name="Pricing">
+        <h1>How much we do it for</h1>
+      </tab>
+      <tab name="About Us">
+        <h1>Why we do it</h1>
+      </tab>
+    </tabs>
+  </div>
+</template>
+
+<script>
+import Tabs from '../components/Tabs.vue';
+import Tab from '../components/Tab.vue';
+
+export default {
+  name: 'Test',
+  components: {
+    tabs: Tabs,
+    tab: Tab,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
